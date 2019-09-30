a6c9370b2db5d894efc987e35b80d674438c23e6
krook1024 <krook1024@gmail.com>
2019-09-30 11:32:35 +0200

    new colors

--- para/docs/prelpara.lua
+++ para/docs/prelpara.lua
@@ -1,6 +1,6 @@
 --[[
 
-% Esport Language (PaRa) , first experiments 
+% Esport Language (PaRa) , first experiments
 % This is a rapid prototype for planning Pasigraphy Rhapsody (Paszigráfia Rapszódia, PaRa)
 %
 % prelpara.lua
@@ -26,12 +26,12 @@
 %
 % for vis_prel_para.tex
 % Working title: Visualization of the Language of the Esports Culture: A Preliminary Study
-% 
+%
 % A LuaLaTeX based visualization experiment for Pasigraphy Rhapsody
 %
-% The idea of using yslant and xslant to achieve a 3D effect like appearance 
+% The idea of using yslant and xslant to achieve a 3D effect like appearance
 % comes from Stefan Kottwitz's example http://www.texample.net/tikz/examples/sudoku-3d-cube/
-% 
+%
 %
 --]]
 function para2D(fmts)
@@ -44,16 +44,16 @@ function para2D(fmts)
             fmts_shift = fmts_shift +1
             nod=sntnc[fmts_shift]
             tex.sprint(
-            		"\\tikzstyle{shifting}=[xshift=", i,"cm]"         			
-					,"\\shade[shifting, left color=gray!10, right color=black!80] (0,0) rectangle +(1,1);"
-					,"\\draw[shifting, step=\\N{",n,"}cm, cyan] (0,0) grid (1,1);")		
+            		"\\tikzstyle{shifting}=[xshift=", i,"cm]"
+					,"\\shade[shifting, left color=mygray!100, right color=myblack!100] (0,0) rectangle +(1,1);"
+					,"\\draw[shifting, step=\\N{",n,"}cm, mycyan] (0,0) grid (1,1);")
             for j = 1, nod do
             	fmts_shift = fmts_shift +1
             	x=sntnc[fmts_shift]
             	fmts_shift = fmts_shift +1
             	y=sntnc[fmts_shift]+1
 				tex.sprint(
-					"\\node[shifting, fill=orange, anchor=north west,  inner sep=0pt,minimum size=\\N{",n,"}cm-\\pgflinewidth] at \\D{",n,"}{",x,"}{",y,"} {};")
+					"\\node[shifting, fill=myorange, anchor=north west,  inner sep=0pt,minimum size=\\N{",n,"}cm-\\pgflinewidth] at \\D{",n,"}{",x,"}{",y,"} {};")
             end
 		end
 	end
@@ -70,56 +70,56 @@ function para3D(direct, fmts)
             fmts_shift = fmts_shift +1
             nod=sntnc[fmts_shift]
             tex.sprint(
-            "\\tikzstyle{leftslant}=[yslant=-.5, xshift=",-1+i*d[dir][1],"cm, yshift=",-1+i*d[dir][2],"cm]"
-			,"\\tikzstyle{rightslant}=[yslant=0.5, xshift=",i*d[dir][3],"cm, yshift=",-1+i*d[dir][4],"cm]"
-			,"\\tikzstyle{topslant}=[yslant=0.5, xslant=-1, xshift=", i*d[dir][5],"cm, yshift=", i*d[dir][6],"cm]"
-			,"\\shade[leftslant, right color=gray!10,left color=black!80] (0,0) rectangle +(1,1);"
-			,"\\draw[leftslant, step=\\N{",n,"}cm, cyan] (0,0) grid (1,1);")
+            "\\tikzstyle{leftslant}=[yslant=-.5, xshift=",-1+i*d[dir][1],"cm, yshift=",-1+i*d[dir][2],"cm, blur shadow={shadow blur steps=5}]"
+			,"\\tikzstyle{rightslant}=[yslant=0.5, xshift=",i*d[dir][3],"cm, yshift=",-1+i*d[dir][4],"cm, blur shadow={shadow blur steps=5}]"
+			,"\\tikzstyle{topslant}=[yslant=0.5, xslant=-1, xshift=", i*d[dir][5],"cm, yshift=", i*d[dir][6],"cm, blur shadow={shadow blur steps=5}]"
+			,"\\shade[leftslant, right color=mygray!100,left color=myblack!100] (0,0) rectangle +(1,1);"
+			,"\\draw[leftslant, step=\\N{",n,"}cm, mycyan] (0,0) grid (1,1);")
             for j = 1, nod do
             	fmts_shift = fmts_shift +1
             	x=sntnc[fmts_shift]
             	fmts_shift = fmts_shift +1
             	y=sntnc[fmts_shift]+1
             	tex.sprint(
-            	"\\node[leftslant, fill=orange, anchor=north west,  inner sep=0pt,minimum size=\\N{",n,"}cm-\\pgflinewidth] at \\D{",n,"}{",x,"}{",y,"-.5*",x,"} {};")
-            end    
-            fmts_shift = fmts_shift +1    
+            	"\\node[leftslant, fill=myorange, anchor=north west,  inner sep=0pt,minimum size=\\N{",n,"}cm-\\pgflinewidth] at \\D{",n,"}{",x,"}{",y,"-.5*",x,"} {};")
+            end
+            fmts_shift = fmts_shift +1
             n=sntnc[fmts_shift]
             fmts_shift = fmts_shift +1
 	        nod=sntnc[fmts_shift]
 			tex.sprint(
-				"\\shade[rightslant, right color=black!70,left color=gray!20] (0,0) rectangle +(1,1);",
-				"\\draw[rightslant, step=\\N{",n,"}cm, pink](0,0) grid (1,1);")
+				"\\shade[rightslant, right color=mygray!100,left color=myblack!100] (0,0) rectangle +(1,1);",
+				"\\draw[rightslant, step=\\N{",n,"}cm, mypink](0,0) grid (1,1);")
 			for j = 1, nod do
             	fmts_shift = fmts_shift +1
 				x=sntnc[fmts_shift]
             	fmts_shift = fmts_shift +1
 				y=sntnc[fmts_shift]
 				tex.sprint(
-					"\\node[rightslant, fill=cyan, anchor=south west,  inner sep=0pt,minimum size=\\N{",n,"}cm-\\pgflinewidth] at \\D{",n,"}{",x,"}{",y,"+.5*",x,"} {};")
-            end    
+					"\\node[rightslant, fill=mycyan, anchor=south west,  inner sep=0pt,minimum size=\\N{",n,"}cm-\\pgflinewidth] at \\D{",n,"}{",x,"}{",y,"+.5*",x,"} {};")
+            end
             fmts_shift = fmts_shift +1
 			n=sntnc[fmts_shift]
 			fmts_shift = fmts_shift +1
-            nod=sntnc[fmts_shift]		
+            nod=sntnc[fmts_shift]
 			tex.sprint(
-				"\\shade[topslant, bottom color=gray!10,top color=black!80] (0,0) rectangle +(1,1);",
-				"\\draw[topslant, step=\\N{",n,"}cm, orange] (0,0) grid (1,1);")
+				"\\shade[topslant, bottom color=mygray!100,top color=myblack!100] (0,0) rectangle +(1,1);",
+				"\\draw[topslant, step=\\N{",n,"}cm, myorange] (0,0) grid (1,1);")
             for j = 1, nod do
            		fmts_shift = fmts_shift +1
             	x=sntnc[fmts_shift]
             	fmts_shift = fmts_shift +1
-				y=sntnc[fmts_shift]	    
+				y=sntnc[fmts_shift]
 				tex.sprint(
-					"\\node[topslant, fill=yellow, anchor=south west,  inner sep=0pt,minimum size=\\N{",n,"}cm-\\pgflinewidth] at \\D{",n,"}{",y,"-",x,"}{",tonumber(y)/2,"+.5*",x,"} {};")
-           end    
+					"\\node[topslant, fill=myyellow, anchor=south west,  inner sep=0pt,minimum size=\\N{",n,"}cm-\\pgflinewidth] at \\D{",n,"}{",y,"-",x,"}{",tonumber(y)/2,"+.5*",x,"} {};")
+           end
 	end
 end
 
-function N(n) 
+function N(n)
             tex.sprint(1/n)
-end 
+end
 
-function D(n, x, y) 
+function D(n, x, y)
             tex.sprint("(",       (1/n)*x,     ", ",   (1/n)*y,       ")")
-end 
+end
